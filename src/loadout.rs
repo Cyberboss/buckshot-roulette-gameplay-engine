0d0ff9a3013c6bdd95b16fc6f53588b9ed3c6a03
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-11-21 21:42:02 -0500

    Loadout sizes from decompilation

--- src/loadout.rs
+++ src/loadout.rs
@@ -2,6 +2,8 @@ use std::ops::Range;
 
 use rand::Rng;
 
+use crate::multiplayer_count::MultiplayerCount;
+
 pub struct Loadout {
     pub initial_blank_rounds: usize,
     pub initial_live_rounds: usize,
@@ -9,7 +11,7 @@ pub struct Loadout {
 }
 
 impl Loadout {
-    pub fn new<TRng>(rng: &mut TRng) -> Self
+    pub fn new<TRng>(multiplayer_count: MultiplayerCount, rng: &mut TRng) -> Self
     where
         TRng: Rng,
     {
@@ -23,8 +25,14 @@ impl Loadout {
 
         let initial_blank_rounds = total_shells - initial_live_rounds;
 
-        // TODO: Validate item count possibilities
-        let new_items = rng.gen_range(Range { start: 2, end: 5 });
+        // https://github.com/thecatontheceiling/buckshotroulette_multiplayer/blob/aed4aecb7fd7f6cec14a7bd17239e736039915c0/global%20scripts/MP_RoundManager.gd#L528
+        let items_range = match multiplayer_count {
+            MultiplayerCount::Two => Range { start: 2, end: 5 },
+            MultiplayerCount::Three => Range { start: 3, end: 6 },
+            MultiplayerCount::Four => Range { start: 3, end: 5 },
+        };
+
+        let new_items = rng.gen_range(items_range);
 
         Loadout {
             initial_blank_rounds,
