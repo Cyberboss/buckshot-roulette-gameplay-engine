b5c9cd51b17e71f33b8fdcb6d9b97240f9977500
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-11-23 10:40:08 -0500

    Implement display for RoundNumber

--- src/round_number.rs
+++ src/round_number.rs
@@ -1,6 +1,19 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum RoundNumber {
     One = 1,
     Two = 2,
     Three = 3,
+}
+
+impl Display for RoundNumber {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            RoundNumber::One => "1",
+            RoundNumber::Two => "2",
+            RoundNumber::Three => "3",
+        };
+        write!(f, "{}", str)
+    }
 }
