3087683434a30f9d1c5abe23cf2acfbdf9305a12
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-11-22 22:39:36 -0500

    Add four player test case

---
+++ tests/all_four_shoot_self.rs
@@ -0,0 +1,45 @@
+use buckshot_roulette_gameplay_engine::{
+    game_session::GameSession, multiplayer_count::MultiplayerCount, round::RoundContinuation,
+};
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+
+#[test]
+fn all_four_shoot_self() {
+    let rng: ChaCha8Rng = ChaCha8Rng::seed_from_u64(42);
+
+    let mut session: GameSession<ChaCha8Rng> = GameSession::new(MultiplayerCount::Four, rng);
+    play_round_one_shoots_two_two_shoots_self(&mut session);
+    play_round_one_shoots_two_two_shoots_self(&mut session);
+    play_round_one_shoots_two_two_shoots_self(&mut session);
+
+    assert!(session.round().is_none());
+}
+
+fn play_round_one_shoots_two_two_shoots_self(session: &mut GameSession<ChaCha8Rng>) {
+    let mut won_round = false;
+    for _ in 0..100 {
+        let current_player = session.round().unwrap().next_player();
+        session
+            .with_turn(
+                |turn| turn.shoot(current_player),
+                |summary| {
+                    assert!(summary.shot_result.is_some());
+                    match &summary.round_continuation {
+                        RoundContinuation::RoundContinues(_) => {}
+                        RoundContinuation::RoundEnds(finished_round) => {
+                            assert!(finished_round.winner() != current_player);
+                            won_round = true;
+                        }
+                    }
+                },
+            )
+            .unwrap();
+
+        if won_round {
+            break;
+        }
+    }
+
+    assert!(won_round)
+}
