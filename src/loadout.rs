e5ffcfa2d554e45049f72996f22016ded8bd9df0
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-11-22 23:28:49 -0500

    Fix sequence RNG

--- src/loadout.rs
+++ src/loadout.rs
@@ -2,6 +2,14 @@ use rand::Rng;
 
 use crate::{multiplayer_count::MultiplayerCount, LOG_RNG};
 
+pub const MAX_SHELLS: u32 = 8;
+
+#[derive(Debug, Clone, Copy)]
+struct Sequence {
+    num_live: usize,
+    num_blank: usize,
+}
+
 pub struct Loadout {
     pub initial_blank_rounds: usize,
     pub initial_live_rounds: usize,
@@ -13,26 +21,14 @@ impl Loadout {
     where
         TRng: Rng,
     {
-        // TODO: Validate shell possiblities
-        let total_shells = rng.gen_range(2, 9);
-
-        if LOG_RNG {
-            println!("Generating {} shells", total_shells);
-        }
-
-        let initial_live_rounds = rng.gen_range(1, total_shells);
-
-        if LOG_RNG {
-            println!("{}/{} live rounds", initial_live_rounds, total_shells);
-        }
-
-        let initial_blank_rounds = total_shells - initial_live_rounds;
+        let sequence = get_sequence(multiplayer_count, rng);
 
         // https://github.com/thecatontheceiling/buckshotroulette_multiplayer/blob/aed4aecb7fd7f6cec14a7bd17239e736039915c0/global%20scripts/MP_RoundManager.gd#L528
+        // #4 overridden in mp_main.tscn
         let new_items = match multiplayer_count {
             MultiplayerCount::Two => rng.gen_range(2, 5),
             MultiplayerCount::Three => rng.gen_range(3, 6),
-            MultiplayerCount::Four => rng.gen_range(3, 5),
+            MultiplayerCount::Four => rng.gen_range(2, 5),
         };
 
         if LOG_RNG {
@@ -40,9 +36,70 @@ impl Loadout {
         }
 
         Loadout {
-            initial_blank_rounds,
-            initial_live_rounds,
+            initial_blank_rounds: sequence.num_blank,
+            initial_live_rounds: sequence.num_live,
             new_items,
         }
+    }
+}
+
+fn get_sequence<TRng>(multiplayer_count: MultiplayerCount, rng: &mut TRng) -> Sequence
+where
+    TRng: Rng,
+{
+    // found in mp_main.tscn
+    let sequences = match multiplayer_count {
+        MultiplayerCount::Two => vec![
+            s(1, 1),
+            s(1, 2),
+            s(2, 1),
+            s(2, 2),
+            s(2, 3),
+            s(3, 1),
+            s(3, 2),
+            s(3, 3),
+            s(4, 2),
+        ],
+        MultiplayerCount::Three => vec![
+            s(1, 1),
+            s(2, 2),
+            s(2, 3),
+            s(3, 1),
+            s(3, 2),
+            s(3, 3),
+            s(3, 4),
+            s(4, 2),
+            s(4, 3),
+            s(4, 4),
+        ],
+        MultiplayerCount::Four => vec![
+            s(2, 1),
+            s(2, 2),
+            s(3, 1),
+            s(3, 2),
+            s(3, 3),
+            s(3, 4),
+            s(3, 4),
+            s(4, 2),
+            s(4, 3),
+            s(4, 4),
+        ],
+    };
+
+    let sequence_index = rng.gen_range(0, sequences.len());
+    if LOG_RNG {
+        println!(
+            "Selecting sequence index {}/{}",
+            sequence_index,
+            sequences.len()
+        );
+    }
+    sequences[sequence_index]
+}
+
+fn s(num_live: usize, num_blank: usize) -> Sequence {
+    Sequence {
+        num_live,
+        num_blank,
     }
 }
