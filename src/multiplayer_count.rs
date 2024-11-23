4f80e3d38d11692ccd3ce56e50efc7a97d38687c
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-11-23 10:41:44 -0500

    Implement display for MultiplayerCount

--- src/multiplayer_count.rs
+++ src/multiplayer_count.rs
@@ -1,6 +1,19 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum MultiplayerCount {
     Two = 2,
     Three = 3,
     Four = 4,
+}
+
+impl Display for MultiplayerCount {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            MultiplayerCount::Four => "4",
+            MultiplayerCount::Two => "2",
+            MultiplayerCount::Three => "3",
+        };
+        write!(f, "{}", str)
+    }
 }
